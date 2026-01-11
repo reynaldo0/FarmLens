1131a19a487fc9a318403b703d25a9b846672c97
reynaldo0 <reynaldooyusellino@gmail.com>
2026-01-11 18:49:44 +0700

    upd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -117,7 +117,7 @@ export function Navbar() {
 
                     {/* Dashboard Button */}
                     <NavLink
-                        to="/dashboard"
+                        to="/dashboard/overview"
                         className="
             flex items-center gap-2
             rounded-xl px-4 py-2
